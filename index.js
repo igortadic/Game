6571dfe908c6e9e467e79d9dfe8b834875e4c241
TehStud <igortadic.msc@gmail.com>
2022-08-17 16:48:08 +0200

    animating characters 3

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ class Sprite {
 
    update() {
      this.draw();
+     this.position.x += this.velocity.x;
      this.position.y += this.velocity.y;
 
      if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -68,3 +69,27 @@ function animate() {
 }
 
 animate();
+
+window.addEventListener('keydown', (event) => {
+  switch(event.key) {
+    case 'd':
+      player.velocity.x = 1;
+    break
+    case 'a':
+      player.velocity.x = -1;
+    break
+  }
+  console.log(event.key)
+});
+
+window.addEventListener('keyup', (event) => {
+  switch(event.key) {
+    case 'd':
+      player.velocity.x = 0;
+    break
+    case 'a':
+      player.velocity.x = 0;
+    break
+  }
+  console.log(event.key)
+})
