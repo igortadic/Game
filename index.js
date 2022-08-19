e1da9b8f61ab67b59da00c1bd8c7f1df825110bb
TehStud <igortadic.msc@gmail.com>
2022-08-19 20:34:21 +0200

    animating characters 4: setting movement across X axis

--- index.js
+++ index.js
@@ -60,12 +60,27 @@ enemy.draw();
 
 console.log(player);
 
+const keys = {
+  a: {
+    pressed: false;
+  },
+  d: {
+    pressed: false;
+  }
+}
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
+
+  if (keys.a.pressed) {
+    player.velocity.x = - 1;
+  } else if (keys.d.pressed) {
+    player.velocity.x = 1;
+  }
 }
 
 animate();
@@ -73,10 +88,10 @@ animate();
 window.addEventListener('keydown', (event) => {
   switch(event.key) {
     case 'd':
-      player.velocity.x = 1;
+      keys.d.pressed = True;
     break
     case 'a':
-      player.velocity.x = -1;
+      keys.a.pressed = True;
     break
   }
   console.log(event.key)
@@ -85,10 +100,10 @@ window.addEventListener('keydown', (event) => {
 window.addEventListener('keyup', (event) => {
   switch(event.key) {
     case 'd':
-      player.velocity.x = 0;
+      keys.d.pressed = False;
     break
     case 'a':
-      player.velocity.x = 0;
+      keys.a.pressed = False;
     break
   }
   console.log(event.key)
