ad14074f48e806dc05a55f293fc8013b9e00ff61
TehStud <igortadic.msc@gmail.com>
2022-08-21 16:19:02 +0200

    working on a background sprite 2

--- index.js
+++ index.js
@@ -43,6 +43,45 @@ class Sprite {
 
    update() {
      this.draw();
+    }
+
+}
+
+class Fighter {
+   constructor({position, velocity, color = 'red', offset}) {
+     this.position = position;
+     this.velocity = velocity;
+     this.width = 50;
+     this.height = 150;
+     this.lastKey
+     this.attackBox = {
+       position: {
+        x: this.position.x,
+        y: this.position.y
+      },
+      offset,
+      width: 100,
+      height: 50
+     }
+     this.isAttacking
+     this.color = color
+     this.health = 100;
+   }
+
+   draw() {
+     c.fillStyle = this.color;
+     c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+     // attackBox
+     if (this.isAttacking) {
+       c.fillStyle = 'green';
+       c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
+     }
+
+   }
+
+   update() {
+     this.draw();
      this.attackBox.position.x = this.position.x + this.attackBox.offset.x
      this.attackBox.position.y = this.position.y
 
