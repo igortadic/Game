169545ff6e504a4430dcf43d4ad1bc0a47f06433
TehStud <igortadic.msc@gmail.com>
2022-08-21 12:28:01 +0200

    Health bar mechanics

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
      }
      this.isAttacking
      this.color = color
+     this.health = 100;
    }
 
    draw() {
@@ -157,6 +158,8 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false
+    enemy.health -= 20
+    document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     console.log('go')
   }
 
@@ -168,6 +171,8 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false
+    player.health -= 20
+    document.querySelector('#playerHealth').style.width = player.health + '%'
     console.log('enemy attack succesful')
   }
 }
