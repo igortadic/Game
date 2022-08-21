85046b10301f41960f22217e0bc5becb3681365f
TehStud <igortadic.msc@gmail.com>
2022-08-21 13:26:07 +0200

    stopping timer upon win

--- index.js
+++ index.js
@@ -126,10 +126,11 @@ function RectangularCollision({ rectangle1, rectangle2 }) {
   )
 }
 
-function determineWinner({player, enemy}) {
+function determineWinner({player, enemy, timerId}) {
+  clearTimeout(timerId)
+  document.querySelector('#displayText').style.display = 'flex'
   if (player.health === enemy.health) {
     document.querySelector('#displayText').innerHTML = 'Tie'
-
   } else if (player.health > enemy.health) {
     document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
   } else if (player.health < enemy.health) {
@@ -139,14 +140,15 @@ function determineWinner({player, enemy}) {
 
 
 let timer = 60;
+let timerId
 function decreaseTimer() {
   if (timer>0) {
-    setTimeout(decreaseTimer, 1000)
+    timerId = setTimeout(decreaseTimer, 1000)
     timer--;
     document.querySelector('#timer').innerHTML = timer;
   }
   if (timer === 0) {
-    document.querySelector('#displayText').style.display = 'flex'
+    determineWinner({player, enemy, timerId})
   }
 }
 
@@ -204,7 +206,7 @@ function animate() {
 
   // end game if health === 0
   if(enemy.health <=0 || player.health <=0) {
-
+    determineWinner({player, enemy, timerId})
   }
 }
 
