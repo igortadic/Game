0d328ae09c850de0eef7f0f846977bd553fe3e20
TehStud <igortadic.msc@gmail.com>
2022-08-23 00:18:04 +0200

    Seperating all non essential functions into a seperate file

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ const player = new Fighter({
   }
 });
 
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0
+  },
+  imageSrc: './img/background.png'
+})
 
 const enemy = new Fighter({
   position: {
@@ -62,46 +69,13 @@ const keys = {
   }
 }
 
-function RectangularCollision({ rectangle1, rectangle2 }) {
-  return (
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x
-    && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
-    && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  )
-}
-
-function determineWinner({player, enemy, timerId}) {
-  clearTimeout(timerId)
-  document.querySelector('#displayText').style.display = 'flex'
-  if (player.health === enemy.health) {
-    document.querySelector('#displayText').innerHTML = 'Tie'
-  } else if (player.health > enemy.health) {
-    document.querySelector('#displayText').innerHTML = 'Player 1 Wins'
-  } else if (player.health < enemy.health) {
-    document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
-  }
-}
-
-
-let timer = 60;
-let timerId
-function decreaseTimer() {
-  if (timer>0) {
-    timerId = setTimeout(decreaseTimer, 1000)
-    timer--;
-    document.querySelector('#timer').innerHTML = timer;
-  }
-  if (timer === 0) {
-    determineWinner({player, enemy, timerId})
-  }
-}
-
 decreaseTimer()
 
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = 'black';
   c.fillRect(0, 0, canvas.width, canvas.height);
+  background.update();
   player.update();
   enemy.update();
 
